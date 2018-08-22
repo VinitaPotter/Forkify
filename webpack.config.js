fb2af11265f2df09ed2f450cfffc2b4f79fcb07d
Vinita <vinitavkumbhar@gmail.com>
2018-08-22 23:31:33 +0530

    Changed webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: ['babel-polyfill', './src/js/index.js'],
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         filename: 'js/bundle.js'
     },
 
+    
     devServer: {
-        contentBase: './dist'
+        contentBase: './docs'
     },
 
     plugins: [
